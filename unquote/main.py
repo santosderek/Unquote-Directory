08a97bd0595e620782e86270bda71777d5e6094e
Derek Santos <derekhalo@gmail.com>
2017-08-31 12:23:57 -0400

    Added ability to unquote directories and added recursion.
    Added the ability to unquote directories as well as improved screen
    output. In addition, the program is more reliable due to using absolute
    paths instead of relative.
    
    Lasty, I have added recursion by using the -r command.

--- unquote/main.py
+++ unquote/main.py
@@ -2,35 +2,76 @@
 
 # Python Modules
 from urllib.parse import unquote
-from os import listdir, rename
+from os import listdir, rename, walk, getcwd
 from os.path import isfile, join
 from argparse import ArgumentParser
 import logging
 
 # Equipped a logger for debugging
-logging.basicConfig(filename='logger.log', level=logging.DEBUG)
+LOG_FORMAT = "[%(levelname)s] - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+
 LOGGER = logging.getLogger()
 
 
 def parse_arguments():
     """ Parses arguments given at startup """
     parser = ArgumentParser(description='Decodes filenames with url encoding.')
-    parser.add_argument('folder', metavar='folder',
+    parser.add_argument('folder', metavar='folder', nargs='?',
                         type=str, help='Target folder')
+    parser.add_argument('-r', '--recursion',
+                        action='store_true', help='Enable recursion.')
     return parser.parse_args()
-
-
-def change_name(name: str):
-    """ Unqote and strip invalid characters from a filename and returns it """
-    unquoted_name = unquote(name)
-    stripped_name = unquoted_name.strip('/\\:*?"<>|')
-    return stripped_name
 
 
 def get_list_of_file_names(directory_path):
     """ Gets a list of files from a path """
     return [filename for filename in listdir(directory_path)
             if isfile(join(directory_path, filename))]
+
+
+def unquote_name(name: str):
+    """ Unqote and strip invalid characters from a filename and returns it """
+    unquoted_name = unquote(name)
+    stripped_name = unquoted_name.strip('/\\:*?"<>|')
+    return stripped_name
+
+
+def unquote_files(directory_path: str):
+    """ Unquote a list of files from a specified directory """
+
+    # Create two lists: One to contain original names,
+    # and a second to contain changed names
+    original_list = get_list_of_file_names(directory_path)
+    unquoted_list = [unquote_name(str(name)) for name in original_list]
+
+    # Loop through both lists and change the names accordingly
+    for original_name, changed_name in zip(original_list, unquoted_list):
+        try:
+            # Renames a file using the path + filename
+            rename(join(directory_path, original_name),
+                   join(directory_path, changed_name))
+
+            LOGGER.info('Changed %s to %s' % (join(directory_path, original_name),
+                                              join(directory_path, changed_name)))
+        except Exception as e:
+            LOGGER.debug('ERROR: ' + e)
+
+
+def unquote_directory_name(path: str):
+    for directory, _, _ in walk(path, followlinks=False):
+        if directory != '.':
+
+            try:
+                changed_directory_name = unquote_name(directory)
+                # Renames a file using the path + filename
+                rename(join(path, directory),
+                       join(path, changed_directory_name))
+
+                LOGGER.info('Changed %s to %s' %
+                            (directory, changed_directory_name))
+            except Exception as e:
+                LOGGER.debug('ERROR: ' + str(e))
 
 
 def main():
@@ -40,26 +81,25 @@ def main():
     arguments = parse_arguments()
 
     # If no argument passed then use working directory
-    directory_path = str(arguments.folder)
+    if arguments.folder is None:
+        path = getcwd()
+    else:
+        path = str(arguments.folder)
+        if path == '.' or path == './':
+            path = getcwd()
 
-    # Create two lists: One to contain original names,
-    # and a second to contain changed names
-    original_list = get_list_of_file_names(directory_path)
-    stripped_list = [change_name(str(name)) for name in original_list]
+    # If recursion argument was present, unquote all sub directories
+    if arguments.recursion:
 
-    # Loop through both lists and change the names accordingly
-    for original_name, changed_name in zip(original_list, stripped_list):
-        try:
-            rename(join(directory_path, original_name),
-                   join(directory_path, changed_name))
+        # Unquote all directory names
+        for directory, _, _ in walk(path):
+            if directory != '.' and join(path, directory) != path:
+                unquote_directory_name(directory)
 
-            print('-------------------------------------------------------')
-            print('Original Name:', original_name)
-            print('Changed Name:', changed_name)
-            print('-------------------------------------------------------')
-        except Exception:
-            LOGGER.info('Could not change %s to %s' %
-                        (original_name, changed_name))
+        # Unquote all file names
+        for directory, _, _ in walk(path):
+            if directory != '.':
+                unquote_files(join(path, directory))
 
 
 if __name__ == '__main__':
